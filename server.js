42a9cba27f3190950aec4cef1da6183518a0e261
Justin Albert <jalbert@carthage.edu>
2020-10-23 22:49:19 -0500

    started post

--- server.js
+++ server.js
@@ -1,26 +1,76 @@
-var express = require("express");
-const { get } = require("http");
-var path=require("path");
+const express = require("express");
+const path=require("path");
+const fs=require("fs");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 
 // Routes
 // ===========================================================
 
-// app.get("/api/notes", function(req, res) {
-//     res.sendFile(path.join(__dirname, "/server/db/db.json"));
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/server/db/db.json"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  }); 
+app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.post("/api/notes", function(req,res){
+    
+
+    
+    fs.readFile("./server/db/db.json", "utf8", function(err,data){
+        if (err) {throw err;}else{
+            let notes=JSON.parse(data);
+            notes.push({"title": "title",
+                        "text": "text",
+                        "id": notes.length
+                    });
+          
+    
+            fs.writeFile("./server/db/db.json",JSON.stringify(notes), function(err){
+                if(err){throw err;}
+            });
+
+
+            res.send(notes);
+        } 
+    });
+    //  let note=req.body; 
+   
+    
+
+    // notes.push(note);
+    // fs.writeFile("./server/db/db.json", JSON.stringify(notes), function(err){
+    //     if (err) throw err;
+    // } );
+ 
+});
+
+// app.delete("/api/notes/:id", function(){
+//     let notes=JSON.parse(fs.readFile("./server/db/db.json", "utf8", function(err){
+//         if (err) throw err;
+//     }));
+//     var id = req.params.id;
+
+//     for(let i=0; i< notes.length; i++){
+//         if(notes[i].id == id){
+//             notes.splice(i,1);
+//         }
+//     }
+//     fs.writeFile("./server/db/db.json", JSON.stringify(notes), function(err){
+//         if (err) throw err;
+//     } );
+//     res.send(`Note deleted.`);
+
 // });
 
-// app.get("/notes", function(req, res) {
-//     res.sendFile(path.join(__dirname, "/public/notes.html"));
-//   }); 
-// app.get("/*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "/public/index.html"));
-// });
 
-get(req,res);
 
 
 // Listener
