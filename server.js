0dd7baecfeaa11047a1c73f7bfda1825c288d04e
Justin Albert <jalbert@carthage.edu>
2020-10-23 19:50:47 -0500

    file set up for heroku

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var path=require("path");
+
+var app = express();
+var PORT = 3000;
+
+
+// Routes
+// ===========================================================
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  }); 
+app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+
+
+
+// Listener
+// ===========================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
